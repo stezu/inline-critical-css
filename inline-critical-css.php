7ccd027a3ddbd5754dfe7c237953f4dbbf543c88
Stephen Zuniga <stephen.zuniga001@gmail.com>
2014-09-06 13:48:57 -0400

    Create plugin file

---
+++ inline-critical-css.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Plugin Name: Inline Critical CSS
+ * Plugin URI: https://github.com/stezu/inline-critical-css
+ * Description: Inline CSS critical to rendering above-the-fold content to improve perceived load time.
+ * Version: 0.1.0
+ * Author: Stephen Zuniga
+ * Author URI: http://stephenzuniga.com
+ *
+ * License: GNU General Public License v2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+class ICCSS {
+	
+	public function __construct() {
+		//
+	}
+}
+
+// Start the plugin up
+new ICCSS();
