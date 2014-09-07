48fa2494e992c466ff66804a12eca88899d29ccf
Stephen Zuniga <stephen.zuniga001@gmail.com>
2014-09-06 22:22:51 -0400

    Move styles to footer
    Include non-blocking js version and no script fallback

--- inline-critical-css.php
+++ inline-critical-css.php
@@ -16,12 +16,12 @@ class ICCSS {
 
 	public function __construct() {
 
+		// Move stylesheets to footer
+		add_action( 'wp_head', array( $this, 'remove_head_stylesheets' ), 2 );
+		add_action( 'wp_footer', array( $this, 'output_footer_stylesheets' ), 1 );
+
 		// Load javascript
 		add_action( 'wp_enqueue_scripts', array( $this, 'load_scripts' ) );
-
-		// Move stylesheets to footer
-		remove_action( 'wp_head', 'wp_enqueue_scripts', 1 );
-		add_action( 'wp_footer', 'wp_enqueue_scripts', 1 );
 	}
 
 	/**
@@ -31,7 +31,59 @@ class ICCSS {
 	 * @return		void
 	 */
 	public function load_scripts() {
-		wp_enqueue_script( 'critical-css', plugins_url( 'assets/critical-css.js', __FILE__ ), array(), '0.1.0', true );
+		wp_enqueue_script( 'critical-css', plugins_url( '/assets/critical-css.js', __FILE__), array(), '0.1.0' );
+	}
+
+	/**
+	 * Remove stylesheets from the head
+	 *
+	 * @access		public
+	 * @return		void
+	 */
+	public function remove_head_stylesheets() {
+		global $wp_styles;
+
+		$this->head_stylesheets = $wp_styles->queue;
+
+		$wp_styles->dequeue( $this->head_stylesheets );
+	}
+
+	/**
+	 * Output styles in the footer
+	 *
+	 * @access		public
+	 * @return		void
+	 */
+	public function output_footer_stylesheets() {
+		global $wp_styles;
+
+		ob_start();
+		$wp_styles->do_items( $this->head_stylesheets );
+		$this->link_items = ob_get_clean();
+
+		if ( $this->link_items ) {
+
+			// Output stylesheets in a non-blocking manner
+			?>
+			<script type="text/javascript">
+				var links = <?php echo json_encode( $this->link_items ); ?>,
+					raf = requestAnimationFrame || mozRequestAnimationFrame ||
+						  webkitRequestAnimationFrame || msRequestAnimationFrame,
+					cb = function () {
+						document.getElementsByTagName("head")[0].insertAdjacentHTML('beforeend', links);
+					};
+
+				if (raf) {
+					raf(cb);
+				} else {
+					window.addEventListener('load', cb);
+				}
+			</script>
+			<?php
+
+			// Output stylesheets in a noscript tag for fallback
+			echo '<noscript>' . $this->link_items . '</noscript>';
+		}
 	}
 }
 
